102c8c779a3ed01cd32d34aadb0153b84e541414
Manu <black_falcon00@hotmail.com>
2016-05-16 17:11:08 +0200

    Ca marche.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "multitexture.h"
+
+#include <QGuiApplication>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication a(argc, argv);
+
+    MyWindow *window = new MyWindow();
+    window->show();
+
+    return a.exec();
+}
